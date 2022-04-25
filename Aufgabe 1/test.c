8db9f3c515c95a1760282e9fa431f3c14d83a575
albenahm <44380401+albenahm@users.noreply.github.com>
2022-04-25 20:02:50 +0200

    add c programm

--- Aufgabe 1/test.c
+++ Aufgabe 1/test.c
@@ -1,11 +1,30 @@
 #include<stdio.h>
 
-int main(){
-unsigned char a= 12;
-unsigned char b = 20;
-unsigned char c = a+b;
+//import the two funcs from the asm Code
+extern int getDivisorCountWithWhileLoop(unsigned int value);
+extern void getDivisorCountWithForLoop(int value, int *result);
 
-printf("the result is %d",c);
+int main(){
+	long long int eingabe;
+	int zeiger=0;
+	int *result=&zeiger;
+	
+	printf("Bitte geben Sie die Eingabe !\n");
+	scanf("%llu", &num);
+	// ueberpruefe der Eingabe 
+	if(eingabe>=0){
+		
+		getDivisorCountWithForLoop(eingabe, result);
+		printf("Das Ergebnis mit For Schleife = %d \n",*result);
+		int ergebnis= getDivisorCountWithWhileLoop(eingabe);
+		rintf("Das Ergebnis mit While Schleife = %d \n",ergebnis);
+		
+	}else if (eingabe > 2147483647){
+		printf("Fehler, falsche Eingabe ,,out of range,,\n");
+	}else{
+		printf("Fehler, falsche Eingabe in negative \n");
+	}
+	
 
 return 0;
 }
